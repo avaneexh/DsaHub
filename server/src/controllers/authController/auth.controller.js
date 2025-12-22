24fc2f66737f9fa19e045c1344fe6a1090eb018a
avaneesh <avaneesh.gupta5@gmail.com>
2025-12-22 23:44:38 +0530

    cookies error

--- server/src/controllers/authController/auth.controller.js
+++ server/src/controllers/authController/auth.controller.js
@@ -37,7 +37,7 @@ export const register = async (req, res) => {
         
         res.cookie("jwt", token, {
             httpOnly:true,
-            sameSite:"strict",
+            sameSite:"none",
             secure:process.env.NODE_ENV !== "development",
             maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days 
         })
@@ -91,7 +91,7 @@ export const login = async (req, res) => {
 
          res.cookie("jwt", token, {
             httpOnly:true,
-            sameSite:"strict",
+            sameSite:"none",
             secure:process.env.NODE_ENV !== "development",
             maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days 
         })
@@ -120,7 +120,7 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie("jwt", {
             httpOnly:true,
-            sameSite:"strict",
+            sameSite:"none",
             secure:process.env.NODE_ENV !== "development"
         })
 
